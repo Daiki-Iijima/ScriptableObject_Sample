08a618b2925f4936d2ba54c96a4db86e004ae967
daiki_iijima <daiki.applecreate@gmail.com>
2019-07-12 22:11:27 +0900

    テストスクリプト追加

---
+++ Assets/ScriptableCharaController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptableCharaController : MonoBehaviour
+{
+
+    public EnemyData enemyData;
+
+    void Start()
+    {
+        ShowScriptableObjectData();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void ShowScriptableObjectData()
+    {
+        // 参照しているEnemyDataの中身をコンソールに表示する
+        Debug.Log("私の名前は" + enemyData.enemyName +
+                    ", 最大HPは" + enemyData.maxHp +
+                    ", 攻撃力は" + enemyData.atk +
+                    ", 防御力は" + enemyData.def +
+                    ", 経験値は" + enemyData.exp +
+                    ", ゴールドは" + enemyData.gold + "です。");
+    }
+}
